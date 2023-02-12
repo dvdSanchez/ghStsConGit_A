55ae315082808b984b823e94960837e9d54c70c5
David Sanchez Bautista <sanchezd@globalhitss.com>
2023-02-12 17:31:58 -0600

    COA Primer cambio en VSanFco

--- src/com/abstrae/stscongit/Main.java
+++ src/com/abstrae/stscongit/Main.java
@@ -3,7 +3,8 @@ package com.abstrae.stscongit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(" Saludos para git ");
+		System.out.println(" Saludos");
+		System.out.println(" Cambio va a subir a la rama VSanFco");
 	}
 
 }
