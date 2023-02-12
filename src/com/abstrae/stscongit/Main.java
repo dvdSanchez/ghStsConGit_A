1e7486a2e6b7a512716716ea80a06e6b6a202cc8
David Sanchez Bautista <sanchezd@globalhitss.com>
2023-02-12 17:37:41 -0600

    COB Ultimo cambio

--- src/com/abstrae/stscongit/Main.java
+++ src/com/abstrae/stscongit/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(" Saludos");
 		System.out.println(" Cambio va a subir a la rama VSanFco");
+		System.out.println("Ultimo en SanFco");
 	}
 
 }
