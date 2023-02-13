af94dc4983bde546e4b80511ed61d0fe81a35238
David Sanchez Bautista <sanchezd@globalhitss.com>
2023-02-13 15:54:24 -0600

    C03 Se indica que es el C03

--- src/com/abstrae/stscongit/Main.java
+++ src/com/abstrae/stscongit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(" Saludos");
 		System.out.println(" Cambio va a subir a la rama VSanFco");
-		System.out.println("Ultimo en SanFco");
+		System.out.println("C03 En Master --Ultimo en SanFco--");
 	}
 
 }
