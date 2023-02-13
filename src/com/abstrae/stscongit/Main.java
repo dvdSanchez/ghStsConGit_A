ff13a77a50759a42c569ee55194de87c71caa70b
David Sanchez Bautista <sanchezd@globalhitss.com>
2023-02-12 20:32:13 -0600

    C03 Cambio en master

--- src/com/abstrae/stscongit/Main.java
+++ src/com/abstrae/stscongit/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(" Saludos");
 		System.out.println(" Cambio va a subir a la rama VSanFco");
-		System.out.println("Ultimo en SanFco");
+		System.out.println("Siempre no fue el Ultimo en SanFco");
+		System.out.println("Cambio en master");
 	}
 
 }
